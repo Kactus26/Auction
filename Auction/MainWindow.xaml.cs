eadda043186d69482ff5f2cf6eaaf9e480370868
Kactus25 <sasha.baginsky@gmail.com>
2024-04-30 09:31:40 +0300

    Добавьте файлы проекта.

---
+++ Auction/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System.Windows;
+
+
+namespace Auction
+{
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+              .WithUrl("https://localhost:7098/chat")
+              .Build();
+        }
+    }
+}
---
+++ AuctionClient/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Windows;
+
+namespace AuctionClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
